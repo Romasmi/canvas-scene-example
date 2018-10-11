3445c52162ed9193346cf7e13b6330d166877c0f
romasmi <romasmi@ro.ru>
2018-10-11 19:36:38 +0300

    lab 2 redone

--- lb2/js/main.js
+++ lb2/js/main.js
@@ -1,3 +1,4 @@
+/*
 (function()
 {
     const dashboard = document.getElementById('dashboard');
@@ -10,18 +11,97 @@
         ctx: ctx
     });
 
-    //graphic.drawStaticPicture();
+    graphic.drawStaticPicture();
+})();
+*/
 
-    const animatedGraphic = new AnimatedGraphic({
-        dashboard: dashboard,
-        ctx: ctx,
-        cloudSpeed: 1
-    });
+(function () {
+    const dashboard = document.getElementById('dashboard');
+    dashboard.width = dashboard.offsetWidth;
+    dashboard.height = dashboard.offsetHeight;
+    const ctx = dashboard.getContext('2d');
 
-    setInterval(function () {
-        animatedGraphic.redraw();
-    }, 17);
+    let entity =
+        {
+            sky: new Rectangle(
+                {
+                    x: 0,
+                    y: 0,
+                    width: dashboard.width,
+                    height: dashboard.height / 3 * 2,
+                    fillColor: '#3c78d8'
+                }),
+            floor: new Rectangle(
+                {
+                    x: 0,
+                    y: dashboard.height / 3 * 2,
+                    width: dashboard.width,
+                    height: dashboard.height / 3,
+                    fillColor: '#6aa84f'
+                }),
+            house: new House(
+                {
+                    bodyX: dashboard.width / 2 - dashboard.width / 8,
+                    bodyY: dashboard.height - dashboard.height / 2 - dashboard.height * 0.05,
+                    width: dashboard.width / 4,
+                    height: dashboard.height / 2,
+                    bodyColor: '#bf9000',
+                    windowColor: '#ffd966',
+                    windowBorderColor: '#000',
+                    roofColor: '#c00',
+                    trumpetColor: '#666666'
+                }),
+            cloud1: new Cloud(
+                {
+                    x: dashboard.width * 0.1,
+                    y: dashboard.height * 0.1,
+                    width: dashboard.width * 0.2,
+                    height: dashboard.height * 0.2,
+                    heightDispersion: 0.05,
+                    widthDispersion: 0.7,
+                    fillColor: '#cfe2f3'
+                }),
+            cloud2: new Cloud(
+                {
+                    x: dashboard.width * 0.4,
+                    y: dashboard.height * 0.1,
+                    width: dashboard.width * 0.2,
+                    height: dashboard.height * 0.2,
+                    heightDispersion: 0.05,
+                    widthDispersion: 0.7,
+                    fillColor: '#cfe2f3'
+                }),
+            cloud3: new Cloud(
+                {
+                    x: dashboard.width * 0.7,
+                    y: dashboard.height * 0.1,
+                    width: dashboard.width * 0.2,
+                    height: dashboard.height * 0.2,
+                    heightDispersion: 0.05,
+                    widthDispersion: 0.7,
+                    fillColor: '#cfe2f3'
+                }),
+            sun: new Sun
+            (
+                {
+                    x: -5,
+                    y: -5,
+                    radius: dashboard.height * 0.15,
+                    fillColor: '#ffbb00'
+                }
+            )
+        };
+    redraw(ctx, entity);
 })();
 
+function redraw(ctx, entity) {
+    drawRectangle(ctx, entity.sky);
+    drawRectangle(ctx, entity.floor);
+    drawHouse(ctx, entity.house);
+    drawCloud(ctx, entity.cloud1);
+    drawCloud(ctx, entity.cloud2);
+    drawCloud(ctx, entity.cloud3);
+    drawSun(ctx, entity.sun);
+}
 
 
